a9fe486cc0e5980f8253f913eb8239f7c3af87e0
iamroshanraut <86304763+iamroshanraut@users.noreply.github.com>
2025-09-10 23:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (58.00%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+
+    let n = nums.length;
+    let totalSum = n * (n+1) / 2;
+
+    let partialSum = 0;
+    for(let i = 0; i < n; i++){
+        partialSum = partialSum + nums[i];
+    }
+
+    return totalSum - partialSum
+    
+    
+}
